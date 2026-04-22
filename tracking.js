d10715355428b5cf07c14e473b173ca6437b49a8
v0sterxx <v0sterx.x@gmail.com>
2026-04-22 22:44:55 +0700

    Create tracking.js

---
+++ tracking.js
@@ -0,0 +1,16 @@
+//Histats
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?3894516&101"/>');
+
+///// Statcounter /////
+var sc_project=13106326; 
+var sc_invisible=1; 
+var sc_security="459fd6ba"; 
+
+document.write('<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>');
+document.write('<noscript>');
+document.write('<div class="statcounter">');
+document.write('<a title="Web Analytics" href="https://statcounter.com/" target="_blank">');
+document.write('<img class="statcounter" src="https://c.statcounter.com/13105535/0/459fd6ba/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
+document.write('</a>');
+document.write('</div>');
+document.write('</noscript>');
