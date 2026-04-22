de525af82766fedda2bf53d47127a901c9eb8ce7
v0sterxx <v0sterx.x@gmail.com>
2026-04-22 22:38:59 +0700

    Create body.js

---
+++ body.js
@@ -0,0 +1,19 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+
+//Paste Iklan Disini
+
+atOptions = {
+    'key' : '66e0e4e3dc880b0342686924fc4f5155',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/66e0e4e3dc880b0342686924fc4f5155/invoke.js"></scr' + 'ipt>');
+
+// Batas Iklan
+
+document.write('</div>');
+document.write('</div>');
