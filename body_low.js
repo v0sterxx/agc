a75f7efb9de4292c2df4cb5a031d4acf183a696d
v0sterxx <v0sterx.x@gmail.com>
2026-04-22 22:43:22 +0700

    Create body_low.js

---
+++ body_low.js
@@ -0,0 +1,19 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+
+//Paste Iklan Disini
+
+atOptions = {
+    'key' : '7b5af7dc0ad614081f71d61b12b5b532',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+};
+
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/7b5af7dc0ad614081f71d61b12b5b532/invoke.js"></scr' + 'ipt>');
+
+// Batas Iklan
+
+document.write('</div>');
+document.write('</div>');
