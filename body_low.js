b685e61daed2e9f4f564dc1189690de6a2a7079a
v0sterxx <v0sterx.x@gmail.com>
2026-05-14 20:59:55 +0700

    Update body_low.js

--- body_low.js
+++ body_low.js
@@ -18,3 +18,26 @@ document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformancefo
 
 document.write('</div>');
 document.write('</div>');
+
+// ======================
+// FLOAT ADS TENGAH 300X250
+// ======================
+
+document.write('<div id="fixedban" style="position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);width:300px;height:280px;text-align:center;overflow:hidden;z-index:999999;">');
+document.write('<div style="width:52px;height:auto;margin:0 auto 5px auto;text-align:center;">');
+document.write('<a onclick="document.getElementById(\'fixedban\').style.display=\'none\';" style="cursor:pointer;">');
+document.write('<img alt="close" src="https://1.bp.blogspot.com/-_A83iDM6JYc/VhtxROLILrI/AAAAAAAADK4/aM4ikIA6aqI/s1600/btn_close.gif" title="close button" style="vertical-align:middle;"/>');
+document.write('</a>');
+document.write('</div>');
+
+document.write('<div style="width:300px;height:250px;margin:0 auto;text-align:center;overflow:hidden;">');
+document.write('<script>');
+document.write('var atOptions = {');
+document.write('key: "4affcbd20c3be4674a94608f282f285d",');
+document.write('format: "iframe",');
+document.write('height: 250,');
+document.write('width: 300,');
+document.write('params: {}');
+document.write('};');
+document.write('<\/script>');
+document.write('<script src="https://www.highperformanceformat.com/4affcbd20c3be4674a94608f282f285d/invoke.js"><\/script>');
